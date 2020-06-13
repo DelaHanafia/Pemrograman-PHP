20e75c3a0526ec4cbb5febb65d5781e588ac1314
DelaHanafia <62319882+DelaHanafia@users.noreply.github.com>
2020-06-13 21:28:46 +0700

    Pemrograman-PHP

---
+++ biodata.php
@@ -0,0 +1,27 @@
+<html>
+<style>
+table, th, td {
+  border: 1px solid black;
+}
+</style>
+<body><table style="width:50%">
+
+	<tr>
+    <td>NIM</td>
+    <td><?php echo $_POST["NIM"]; ?></td> 
+     </tr>
+  <tr>
+    <td>NAMA</td>
+    <td><?php echo $_POST["NAMA"]; ?></td>
+    </tr>
+  <tr>
+    <td>JENIS KELAMIN</td>
+    <td><?php echo $_POST["KELAMIN"]; ?></td>
+    </tr>
+  <tr>
+    <td>PROGRAM STUDI</td>
+    <td><?php echo $_POST["PRODI"]; ?></td>
+     </tr>
+</table>
+</body>
+</html>
